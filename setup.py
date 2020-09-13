d453ccc277b8dcaf2e5682e93340cf93d715eae8
Kato Shinya <yourdream28@gmail.com>
2020-09-13 11:42:55 +0900

    add

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="keywatcher",
+    version='1.0',
+    description='',
+    author='Kato Shinya',
+    author_email='kato.shinya.dev@gmail.com',
+    url='https://github.com/myConsciousness/key-watcher',
+)
